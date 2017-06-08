c00deb7975cedd73b273e09b4ee569465c59fcb9
Oshadha Deemantha <oshadadeemantha@gmail.com>
2017-06-08 16:27:37 +0530

    close test commit

--- app/src/main/java/com/example/oshadhadeemantha/back_to_school/LoginActivity.java
+++ app/src/main/java/com/example/oshadhadeemantha/back_to_school/LoginActivity.java
@@ -10,6 +10,5 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        int name=10;
     }
 }
