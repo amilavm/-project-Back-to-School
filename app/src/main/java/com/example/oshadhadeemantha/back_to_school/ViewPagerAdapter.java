4fce28d2953b5cf79ba8628d9b93d60a9c557514
Oshadha Deemantha <oshadadeemantha@gmail.com>
2017-06-18 23:52:39 +0530

    final done activity

--- app/src/main/java/com/example/oshadhadeemantha/back_to_school/ViewPagerAdapter.java
+++ app/src/main/java/com/example/oshadhadeemantha/back_to_school/ViewPagerAdapter.java
@@ -2,7 +2,11 @@ package com.example.oshadhadeemantha.back_to_school;
 
 import android.content.Context;
 import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
 
 /**
  * Created by OshadhaDeemantha on 6/18/2017.
@@ -11,13 +15,40 @@ import android.view.View;
 public class ViewPagerAdapter extends PagerAdapter {
 
     private Context context;
+    private LayoutInflater layoutInflater;
+    private Integer [] images = {R.drawable.slide_1,R.drawable.slide_2,R.drawable.slide_3};
+
+    public ViewPagerAdapter(Context context) {
+        this.context = context;
+    }
+
     @Override
     public int getCount() {
-        return 0;
+        return images.length;
     }
 
     @Override
     public boolean isViewFromObject(View view, Object object) {
-        return false;
+        return view==object;
+    }
+
+    @Override
+    public Object instantiateItem(ViewGroup container, int position) {
+        layoutInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View view = layoutInflater.inflate(R.layout.custom_layout,null);
+        ImageView imageView = (ImageView) view.findViewById(R.id.imageView);
+        imageView.setImageResource(images[position]);
+
+        ViewPager viewPager = (ViewPager) container;
+        viewPager.addView(view,0);
+        return view;
+    }
+
+    @Override
+    public void destroyItem(ViewGroup container, int position, Object object) {
+
+        ViewPager viewPager = (ViewPager) container;
+        View view = (View) object;
+        viewPager.removeView(view);
     }
 }
