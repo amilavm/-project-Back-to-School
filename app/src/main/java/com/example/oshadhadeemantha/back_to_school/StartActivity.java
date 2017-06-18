0905eb273904d3e8415be8c9ec48e1c97ee6c2da
Oshadha Deemantha <oshadadeemantha@gmail.com>
2017-06-18 23:52:01 +0530

    Add dots and auto slide

--- app/src/main/java/com/example/oshadhadeemantha/back_to_school/StartActivity.java
+++ app/src/main/java/com/example/oshadhadeemantha/back_to_school/StartActivity.java
@@ -1,17 +1,90 @@
 package com.example.oshadhadeemantha.back_to_school;
 
+import android.support.v4.content.ContextCompat;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class StartActivity extends AppCompatActivity {
 
     ViewPager viewPager;
+    LinearLayout sliderDotspanel;
+    private int dotscount;
+    private ImageView [] dots;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
 
         viewPager = (ViewPager) findViewById(R.id.viewPaper);
+
+        sliderDotspanel = (LinearLayout) findViewById(R.id.sliderDots);
+
+
+        ViewPagerAdapter viewPagerAdapter = new ViewPagerAdapter(this);
+
+        viewPager.setAdapter(viewPagerAdapter);
+
+        Timer timer= new Timer();
+        timer.scheduleAtFixedRate(new MyTimerTask(),1000,4000);
+
+        dotscount=viewPagerAdapter.getCount();
+        dots=new ImageView[dotscount];
+
+        for (int i=0;i<dotscount;i++){
+            dots[i]=new ImageView(this);
+            dots[i].setImageDrawable(ContextCompat.getDrawable(getApplicationContext(),R.drawable.nonactive_dot));
+
+            LinearLayout.LayoutParams params= new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT,LinearLayout.LayoutParams.WRAP_CONTENT);
+            params.setMargins(8, 0, 0, 8);
+
+            sliderDotspanel.addView(dots[i],params);
+        }
+        dots[0].setImageDrawable(ContextCompat.getDrawable(getApplicationContext(),R.drawable.active_dot));
+
+        viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+
+                for (int i=0;i<dotscount;i++){
+                    dots[i].setImageDrawable(ContextCompat.getDrawable(getApplicationContext(),R.drawable.nonactive_dot));
+                }
+                dots[position].setImageDrawable(ContextCompat.getDrawable(getApplicationContext(),R.drawable.active_dot));
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
+    }
+
+    public class MyTimerTask extends TimerTask{
+
+        @Override
+        public void run() {
+            StartActivity.this.runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    if(viewPager.getCurrentItem()==0){
+                        viewPager.setCurrentItem(1);
+                    }else if(viewPager.getCurrentItem()==1){
+                        viewPager.setCurrentItem(2);
+                    } else{
+                        viewPager.setCurrentItem(0);
+                    }
+                }
+            });
+        }
     }
 }
